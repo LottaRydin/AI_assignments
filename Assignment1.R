64370fa6a02f85ee232cdc1a77ccce7c93aeb73e
Lotta Rydin <lotta.rydin@telia.com>
2021-09-09 16:24:56 +0200

    grad for road conditions

--- Assignment1.R
+++ Assignment1.R
@@ -1,10 +1,3 @@
-# File:         demo.r 
-# Description:  Naive demo-solution given at classroom session
-#               for Project 1, Artificial Intelligence 2019, UU
-# Author:       Fredrik Nilsson
-
-# Install the package
-# install.packages("DeliveryMan_1.1.0.tar.gz", repos = NULL, type="source")
 
 # Load the library
 library("DeliveryMan")
@@ -19,17 +12,15 @@ myFunction <- function(trafficMatrix, carInfo, packageMatrix) {
     goal <- nextPickup(trafficMatrix, 
                                    carInfo, 
                                    packageMatrix)
-    #print(goal)
+
   } else {
     goal <- packageMatrix[carInfo$load, c(3,4)]
   }
- # print(cat("goal when born", goal))
   # How do we get there?
   carInfo$nextMove <- nextMove(trafficMatrix,
                                carInfo,
                                packageMatrix, 
                                goal)
-  #print("------------------STEP----------------")
   return(carInfo)
 }
 
@@ -48,7 +39,7 @@ man_dist <- function(car_pos, goal_pos) {
 
 # Find the move to get to carInfo$mem$goal
 nextMove <- function(trafficMatrix, carInfo, packageMatrix, goal) {
-  #print("nextMove")
+
   road_con <- (mean(trafficMatrix$vroads) + mean(trafficMatrix$hroads))/2
   grad <- 1
   first_front <- list(x=carInfo$x, y=carInfo$y, f=0, h=man_dist(c(carInfo$x,carInfo$y), goal), p=c())
@@ -56,96 +47,44 @@ nextMove <- function(trafficMatrix, carInfo, packageMatrix, goal) {
   expand <- list()
   
   if (carInfo$x == goal[1] & carInfo$y == goal[2]){
-    # print("same spot!!")
     return(5)
   }
   
   
   while (length(frontier) != 0){
-    #print(cat("lenght frontier:", length(frontier)))
-    # if (length(frontier)>1000){
-    #   print(cat("carinfo: ", c(carInfo$x,carInfo$y)))
-    #   print(cat("goal: ", goal))
-    #   print(cat("expanded coordinats:", c(expand$x,expand$y)))
-    # }
-    # print("nextMove while")
     
     path_vals = sapply(frontier, function(i) i[[3]]+i[[4]])
-    #best_index = which.min(path_vals) # find index of best frontier (basic)
     best_index = rev(which(path_vals == min(path_vals)))[1] # reversed algorithm. Best so far.
-    
-    ## Algorithm that uses best value from manhattan distance:
-    # heu_vals = sapply(frontier, function(i) i[[4]])
-    # print(heu_vals)
-    #best_index = which.min(heu_vals[which.(path_vals == min(path_vals), arr.ind = TRUE)])
-    #print(cat("which(path_vals == min(path_vals)):", which(path_vals == min(path_vals), arr.ind = TRUE)))
-    # best_index = which.min(heu_vals[which.min(path_vals)])
-    # print(cat("which.min(heu_vals[which.min(path_vals, arr.ind = TRUE)]):", which.min(heu_vals[which.min(path_vals)])))
-    # print(cat("best index:", best_index))
           
     expand = frontier[[best_index]]
     frontier = frontier[-best_index] # Pop best frontier
-    #print("expand:")
-    #print(expand)
-    #print("frontier after sapply:")
-    #print(frontier)
     
     if (expand$x == goal[1] & expand$y == goal[2]) {
-      # print(cat("goal when return:", goal))
-      
-      # print(c(expand$x, expand$y))
-      # print(cat("returned value:", expand$p[1]))
       return(expand$p[1])
     } else {
-      # print("vroads:")
-      # print(trafficMatrix$vroads)
       if (ncol(trafficMatrix$vroads) >= expand$y){
-        # print("up")
-        # print(cat("nrows vroads:", nrow(trafficMatrix$vroads)))
-        # print(cat("x an y values for vroads", expand$x, expand$y))
         up <- list(x=expand$x, y=expand$y+1, f=trafficMatrix$vroads[expand$x,expand$y]+expand$f, 
                    h=man_dist(c(expand$x,expand$y+1), goal)*road_con*grad, p=append(expand$p, 8))
         frontier = append(frontier, list(up))
       }
       
       if (expand$y != 1) {
-        #print("down")
         down <- list(x=expand$x, y=expand$y-1, f=trafficMatrix$vroads[expand$x,expand$y-1]+expand$f, 
                      h=man_dist(c(expand$x,expand$y-1), goal)*road_con*grad, p=append(expand$p, 2))
         frontier = append(frontier, list(down))
       }
       
       if (expand$x != 1) {
-        #print("left")
         left <- list(x=expand$x-1, y=expand$y, f=trafficMatrix$hroads[expand$x-1,expand$y]+expand$f, 
                      h=man_dist(c(expand$x-1,expand$y), goal)*road_con*grad, p=append(expand$p, 4))
         frontier = append(frontier, list(left))
       }
       
       if (nrow(trafficMatrix$hroads) >= expand$x) {
-        #print("right")
         right <- list(x=expand$x+1, y=expand$y, f=trafficMatrix$hroads[expand$x,expand$y]+expand$f, 
                       h=man_dist(c(expand$x+1,expand$y), goal)*road_con*grad, p=append(expand$p, 6))
         frontier = append(frontier, list(right))
       }
-      # print("frontier after added directions:")
-      # print(frontier)
     }
   }
-  #while fronties is not empty
-  # move firt element i frontier to expand
-  # check if expand is goal. 
-  # if not: for each neighbour calculate cost of path and insert to frontier (i ordning)
-  
-  # if(carInfo$x < carInfo$mem$goal[1]) {
-  #   return(6)
-  # } else if (carInfo$x > carInfo$mem$goal[1]) {
-  #   return(4)
-  # } else if (carInfo$y < carInfo$mem$goal[2]) {
-  #   return(8)
-  # } else if (carInfo$y > carInfo$mem$goal[2]) {
-  #   return(2)
-  # } else {
-  #   return(5)
-  # }
 }
