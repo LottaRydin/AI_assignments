c5ad86091bd2a1b922421070d7aa2812031b2c73
Vendela Andersson <vendela.a97@gmail.com>
2021-09-10 10:52:03 +0200

    commented version

--- Assignment1.R
+++ Assignment1.R
@@ -2,17 +2,12 @@
 # Load the library
 library("DeliveryMan")
 
-# Read documentation
-# ?runDeliveryMan
-# ?testDM
-
 myFunction <- function(trafficMatrix, carInfo, packageMatrix) {
   # What is our goal?
   if(carInfo$load == 0) {
     goal <- nextPickup(trafficMatrix, 
                                    carInfo, 
                                    packageMatrix)
-
   } else {
     goal <- packageMatrix[carInfo$load, c(3,4)]
   }
@@ -32,60 +27,63 @@ nextPickup <- function(trafficMatrix, carInfo, packageMatrix) {
   return(packageMatrix[which.min(distanceVector), c(1,2)])
 }
 
+# Function for calculating the manhattan distance
 man_dist <- function(car_pos, goal_pos) {
   dis = abs((car_pos[[1]]-goal_pos[[1]])) + abs((car_pos[[2]]-goal_pos[[2]]))
   return(dis)
 }
 
-# Find the move to get to carInfo$mem$goal
+# Find the move to get to goal
 nextMove <- function(trafficMatrix, carInfo, packageMatrix, goal) {
 
-  road_con <- min(c(min(trafficMatrix$vroads), min(trafficMatrix$hroads)))
-  grad <- 1
+  road_con <- min(c(min(trafficMatrix$vroads), min(trafficMatrix$hroads))) #variable for storing traffic condition
   first_front <- list(x=carInfo$x, y=carInfo$y, f=0, h=man_dist(c(carInfo$x,carInfo$y), goal), p=c())
   frontier <- list(first_front)
   expand <- list()
   
+  # If deliveryMan start on goal, it should stay for one move
   if (carInfo$x == goal[1] & carInfo$y == goal[2]){
     return(5)
   }
   
-  
   while (length(frontier) != 0){
-    #print(length(frontier))
+    # Find index for the best node in frontier, move to expand
     path_vals = sapply(frontier, function(i) i[[3]]+i[[4]])
-    best_index = rev(which(path_vals == min(path_vals)))[1] # reversed algorithm. Best so far.
-          
+    best_index = rev(which(path_vals == min(path_vals)))[1]
     expand = frontier[[best_index]]
     frontier = frontier[-best_index] # Pop best frontier
     
+    # Return first move when the best path is found
     if (expand$x == goal[1] & expand$y == goal[2]) {
       return(expand$p[1])
+      
     } else {
+      # Find all neighboring nodes
       add_front = list()
       if (ncol(trafficMatrix$vroads) >= expand$y){
         up <- list(x=expand$x, y=expand$y+1, f=trafficMatrix$vroads[expand$x,expand$y]+expand$f, 
-                   h=man_dist(c(expand$x,expand$y+1), goal)*road_con*grad, p=append(expand$p, 8))
+                   h=man_dist(c(expand$x,expand$y+1), goal)*road_con, p=append(expand$p, 8))
         add_front = append(add_front, list(up))
       }
       
       if (expand$y != 1) {
         down <- list(x=expand$x, y=expand$y-1, f=trafficMatrix$vroads[expand$x,expand$y-1]+expand$f, 
-                     h=man_dist(c(expand$x,expand$y-1), goal)*road_con*grad, p=append(expand$p, 2))
+                     h=man_dist(c(expand$x,expand$y-1), goal)*road_con, p=append(expand$p, 2))
         add_front = append(add_front, list(down))
       }
       
       if (expand$x != 1) {
         left <- list(x=expand$x-1, y=expand$y, f=trafficMatrix$hroads[expand$x-1,expand$y]+expand$f, 
-                     h=man_dist(c(expand$x-1,expand$y), goal)*road_con*grad, p=append(expand$p, 4))
+                     h=man_dist(c(expand$x-1,expand$y), goal)*road_con, p=append(expand$p, 4))
         add_front = append(add_front, list(left))
       }
       
       if (nrow(trafficMatrix$hroads) >= expand$x) {
         right <- list(x=expand$x+1, y=expand$y, f=trafficMatrix$hroads[expand$x,expand$y]+expand$f, 
-                      h=man_dist(c(expand$x+1,expand$y), goal)*road_con*grad, p=append(expand$p, 6))
+                      h=man_dist(c(expand$x+1,expand$y), goal)*road_con, p=append(expand$p, 6))
         add_front = append(add_front, list(right))
       }
+      # Check if node exists in frontier, replace it if current is better
       for (i in 1:length(add_front)){
         if (length(frontier) != 0){
           ex = FALSE
